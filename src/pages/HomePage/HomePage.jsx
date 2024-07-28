d90758a1e79f182913f11307af390228d80131d3
kinderpunk <tsymbal.vladislav@gmail.com>
2024-07-28 16:36:31 +0200

    qwe

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import css from './HomePage.module.css';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className={css.title}>Dobrogo zdorov`ya, koza4e</h1>
+      <p className={css.text}>
+        Na ciy storinci tebe naw4yt yak zhity zhittya
+      </p>
+    </div>
+  );
+}
